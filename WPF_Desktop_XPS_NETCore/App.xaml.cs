1c33d6fb425cf2054060586948fbaaaf262f4243
Moorthy K <moorthy.karunanithi@syncfusion.com>
2024-01-23 23:58:59 +0530

    Added Project for pipeline

---
+++ WPF_Desktop_XPS_NETCore/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPF_Desktop_XPS_NETCore
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
